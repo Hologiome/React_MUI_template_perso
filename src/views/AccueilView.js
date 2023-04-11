ac7a7a81d3d60a62c646f34756ea3dcbb5d47e42
Bastien Beau <202223babas@gmail.com>
2023-04-11 07:26:28 +0000

    Update 2 files

--- src/views/AccueilView.js
+++ src/views/AccueilView.js
@@ -3,7 +3,7 @@ import Container from "@mui/material/Container";
 export default function AccueilView() {
   return (
     <div>
-      <h3>Bienvenue !!</h3>
+      <h3>Bienvenue dans notre application SAE 401 !!</h3>
     </div>
   );
 }
